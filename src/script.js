a57ac2f02657155a93ac9dcc9945c046cedf2768
amarallandre <111255294+amarallandre@users.noreply.github.com>
2023-09-19 21:22:42 -0300

    Primeiro commit

--- src/script.js
+++ src/script.js
@@ -14,33 +14,3 @@ botoesComConteudo.forEach((botaoComConteudo) => {
     }
   });
 });
-
-let galeriaAberta = false;
-let janelaGaleria;
-
-function abrirGaleria() {
-  if (!galeriaAberta) {
-    // Crie uma nova janela (popup) para a galeria
-    janelaGaleria = window.open('', '_blank', 'width=800,height=600');
-    // Defina o conteúdo HTML da galeria na nova janela
-    janelaGaleria.document.body.innerHTML = `
-      <h1>Galeria de Projetos</h1>
-      <img src="imagens/pontoeletronico-inicio.png" alt="Projeto 1">
-      <!-- Adicione mais imagens conforme necessário -->
-      <button onclick="fecharGaleria()">Fechar</button>
-    `;
-    
-    galeriaAberta = true;
-  } else {
-    // Se a galeria já estiver aberta, traga o foco para a janela existente
-    janelaGaleria.focus();
-  }
-}
-
-function fecharGaleria() {
-  if (galeriaAberta) {
-    // Feche a janela da galeria
-    janelaGaleria.close();
-    galeriaAberta = false;
-  }
-}
